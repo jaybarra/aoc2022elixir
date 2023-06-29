cf0d3b85afff2bb9b0edcdaa76046516c77168d0
Jay Barra <jay.barra@nasa.gov>
2023-06-29 00:40:03 +0000

    day 2 part 2

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,49 +1,88 @@
 defmodule Day2 do
   @moduledoc """
 
-  A => rock
-  B => paper
-  C => scissor
+  A/X => rock:     1
+  B/Y => paper:    2
+  C/Z => scissors: 3
 
-  X => rock
-  Y => paper
-  Z => scissor
+  win = 6
+  tie = 3
+  loss = 0
+
+  ## Examples
+
+  iex> Day2.score_game(:rock, :paper)
+  6
+
+  iex> Day2.score_game(:paper, :rock)
+  0
   """
 
-  @rock 1
-  @paper 2
-  @scissor 3
+  def letter_to_symbol(sym) do
+    cond do
+      String.match?(sym, ~r/[AX]/i) -> :rock
+      String.match?(sym, ~r/[BY]/i) -> :paper
+      String.match?(sym, ~r/[CZ]/i) -> :scissor
+    end
+  end
 
-  def play_rock_paper_scissor(gameplan) do
-    gameplan
-    |> Enum.map(fn [them, us] ->
-      usxt =
-        case us do
-          "X" -> @rock
-          "Y" -> @paper
-          "Z" -> @scissor
-        end
+  def letter_to_outcome(sym) do
+    case sym do
+      "X" -> :lose
+      "Y" -> :draw
+      "Z" -> :win
+    end
+  end
 
-      themxt =
-        case them do
-          "A" -> @rock
-          "B" -> @paper
-          "C" -> @scissor
-        end
+  def score_game(them, us) do
+    cond do
+      us == them -> 3
+      us == :rock && them == :scissor -> 6
+      us == :paper && them == :rock -> 6
+      us == :scissor && them == :paper -> 6
+      true -> 0
+    end
+  end
 
-      [themxt, usxt]
-    end)
-    |> Enum.reduce(0, fn [them, us], acc ->
-      win_score =
-        cond do
-          us == them -> 3
-          us == @rock && them == @scissor -> 6
-          us == @paper && them == @rock -> 6
-          us == @scissor && them == @paper -> 6
-          true -> 0
-        end
+  def score_symbol(sym) do
+    case sym do
+      :rock -> 1
+      :paper -> 2
+      :scissor -> 3
+    end
+  end
 
-      us + win_score + acc
-    end)
+  def score_rock_paper_scissor(them, us) do
+    [them, us] = Enum.map([them, us], &letter_to_symbol/1)
+    score_symbol(us) + score_game(them, us)
+  end
+
+  def score_rock_paper_scissor(rounds) do
+    rounds
+    |> Enum.map(fn [a, b] -> score_rock_paper_scissor(a, b) end)
+    |> Enum.sum()
+  end
+
+  def play_rock_paper_scissor_with_expected_outcome(them, outcome) do
+    them_sym = letter_to_symbol(them)
+
+    us =
+      case [them_sym, letter_to_outcome(outcome)] do
+        [:scissor, :win] -> "X"
+        [:scissor, :lose] -> "Y"
+        [:paper, :win] -> "Z"
+        [:paper, :lose] -> "X"
+        [:rock, :win] -> "Y"
+        [:rock, :lose] -> "Z"
+        _ -> them
+      end
+
+    score_rock_paper_scissor(them, us)
+  end
+
+  def play_rock_paper_scissor_with_expected_outcome(rounds) do
+    rounds
+    |> Enum.map(fn [a, b] -> play_rock_paper_scissor_with_expected_outcome(a, b) end)
+    |> Enum.sum()
   end
 end
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -12,12 +12,42 @@ defmodule Day2Test do
     {:ok, test_data: test_data}
   end
 
+  test "validate test data", state do
+    assert length(state[:test_data]) == 2500
+  end
+
+  test "plays a single hand" do
+    assert Day2.score_rock_paper_scissor("A", "X") == 4
+  end
+
+  test "scoring games yields the correct score" do
+    games_to_score = [
+      ["A", "Y", 8],
+      ["B", "X", 1],
+      ["C", "Z", 6],
+      ["A", "X", 4],
+      ["C", "X", 7]
+    ]
+
+    for [them, us, score] <- games_to_score do
+      assert Day2.score_rock_paper_scissor(them, us) == score
+    end
+  end
+
   test "plays and scores rock paper scissor" do
     inputs = [["A", "Y"], ["B", "X"], ["C", "Z"]]
-    assert Day2.play_rock_paper_scissor(inputs) == 15
+    assert Day2.score_rock_paper_scissor(inputs) == 15
   end
 
   test "plays using input file data", state do
-    assert Day2.play_rock_paper_scissor(state[:test_data]) == 12458
+    assert Day2.score_rock_paper_scissor(state[:test_data]) == 12458
+  end
+
+  test "plays using input but expected outcome", state do
+    assert Day2.play_rock_paper_scissor_with_expected_outcome("A", "Y") == 4
+  end
+
+  test "plays using input file data for fixed with", state do
+    assert Day2.play_rock_paper_scissor_with_expected_outcome(state[:test_data]) == 12683
   end
 end
